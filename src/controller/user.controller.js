34ea2f9f3f4ecd5f6405bc146afd9ff73a17ca78
David <cruz-azul99@hotmail.com>
2021-03-08 10:10:10 -0600

    Password encrypted and function logIn created - On function saveUser was added another function to encrypt the password - funciton logIn was created but it is not completed

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,14 +1,17 @@
 const userSchema = require('../model/user.model')
+const encrypt = require('../utils/encrypt')
+const matchPassword = require('../utils/matchPassword')
 
 async function saveUser(req, res) {
-  let user = new userSchema()
-  user.completeName = req.body.completeName
-  user.phone = req.body.phone
-  user.email = req.body.email
-  user.password = req.body.password
-  user.role = req.body.role
-
   try{
+    req.body.password = encrypt(req.body.password,10)
+    let user = new userSchema()
+    user.completeName = req.body.completeName
+    user.phone = req.body.phone
+    user.email = req.body.email
+    user.password = req.body.password
+    user.role = req.body.role
+
     await user.save((error, userAdded) => {
       if(error){
         res.status(500).send({msg: 'error'})
@@ -25,6 +28,16 @@ async function saveUser(req, res) {
   }
 }
 
+async function logIn(req, res) {
+  try {
+    const params = req.body
+    const matchPass = matchPassword(params.password)
+  } catch(err) {
+
+  }
+}
+
 module.exports = {
-  saveUser
+  saveUser,
+  logIn
 }
