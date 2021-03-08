150f515995ce9fff1f84ae60287b7a5f82ee2620
SariMelina <mmkings_beba@hotmail.com>
2021-03-07 19:57:18 -0600

    Corrected Syntax

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const bp = require ('body-parser')
 const route = require('./routes/user.routes')
 
 app.use(bp.urlencoded({extended:false}))
-app.use(bp.json)
+app.use(bp.json())
 
 app.use('/v2', route)
 
