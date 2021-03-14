aa4d5579ace6dd85a03e430f53f2baf625c5dd7f
David <cruz-azul99@hotmail.com>
2021-03-13 19:48:50 -0600

    Middleware verifyId added to the route of the put

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,12 +4,13 @@ const userController = require('../controller/user.controller')
 const verifyEmptyFields = require('../middlewares/verifyEmptyFields')
 const verifyDuplicatesEmails = require('../middlewares/verifyDuplicatesEmails')
 const verifyRole = require('../middlewares/verifyRole')
+const verifyId = require('../middlewares/verifyId')
 
 api.post('/user', verifyRole, verifyEmptyFields, verifyDuplicatesEmails, userController.saveUser)
 api.post('/logIn', userController.logIn)
 api.get('/user', userController.getAllUsers)
 api.get('/user/:matchUser',userController.getMatchUser)
 api.delete('/user/:userId', userController.deleteUser)
-api.put('/user/:userId',userController.updateUserData)
+api.put('/user/:userId?', verifyId, userController.updateUserData)
 
 module.exports = api;
