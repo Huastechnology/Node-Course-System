13df8d768ae1766734c8ece898c4fe8c4a336a33
SariMelina <mmkings_beba@hotmail.com>
2021-03-24 19:48:38 -0600

    Importing the middlewares and adding to de routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,16 +1,13 @@
 const { Router } = require('express')
 const api = Router()
 const userController = require('../controller/user.controller')
-const verifyEmptyFields = require('../middlewares/verifyEmptyFields')
-const verifyDuplicatesEmails = require('../middlewares/verifyDuplicatesEmails')
-const verifyRole = require('../middlewares/verifyRole')
-const verifyId = require('../middlewares/verifyId')
+const middlewares = require('../middlewares/index')
 
-api.post('/user', verifyRole, verifyEmptyFields, verifyDuplicatesEmails, userController.saveUser)
+api.post('/user',middlewares.verifyRole, middlewares.verifyEmptyFields, middlewares.verifyDuplicatesEmails, userController.saveUser)
 api.post('/logIn', userController.logIn)
 api.get('/user', userController.getAllUsers)
 api.get('/user/:matchUser',userController.getMatchUser)
 api.delete('/user/:userId', userController.deleteUser)
-api.put('/user/:userId?', verifyId, userController.updateUserData)
+api.put('/user/:userId?', middlewares.verifyId, userController.updateUserData)
 
 module.exports = api;
