40255e6d9e03ef76280c53bf3d4a50922f3325aa
SariMelina <mmkings_beba@hotmail.com>
2021-03-24 22:05:33 -0600

    Adding to the 'delete' the verifyRole middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,14 @@
 const { Router } = require('express')
 const api = Router()
 const userController = require('../controller/user.controller')
+const { verifyRole } = require('../middlewares/index')
 const middlewares = require('../middlewares/index')
 
 api.post('/user',middlewares.verifyRole, middlewares.verifyEmptyFields, middlewares.verifyDuplicatesEmails, userController.saveUser)
 api.post('/logIn', userController.logIn)
 api.get('/user', middlewares.verifyAuth, userController.getAllUsers)
 api.get('/user/:matchUser', middlewares.verifyAuth ,userController.getMatchUser)
-api.delete('/user/:userId', userController.deleteUser)
+api.delete('/user/:userId',middlewares.verifyRole ,userController.deleteUser)
 api.put('/user/:userId?', middlewares.verifyAuth ,middlewares.verifyId, userController.updateUserData)
 
 module.exports = api;
