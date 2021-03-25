d2b9528e4dd50939fadac8fcec10723c2c0144db
SariMelina <mmkings_beba@hotmail.com>
2021-03-24 23:37:25 -0600

    Adding the new middleware (verifyUserAccess)

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -3,11 +3,13 @@ const verifyDuplicatesEmails = require('../middlewares/verifyDuplicatesEmails')
 const verifyRole = require('../middlewares/verifyRole')
 const verifyId = require('../middlewares/verifyId')
 const verifyAuth = require('../middlewares/verifyAuth')
+const verifyUserAccess = require('../middlewares/verifyUserAccess')
 
 module.exports = {
   verifyEmptyFields,
   verifyDuplicatesEmails,
   verifyRole,
   verifyId,
-  verifyAuth
+  verifyAuth,
+  verifyUserAccess
 }
