559ccefd732e3313441ed4493e9d15423e4d7752
SariMelina <mmkings_beba@hotmail.com>
2021-03-24 23:41:00 -0600

    Creating verifyUserAccess middleware

---
+++ src/middlewares/verifyUserAccess.js
@@ -0,0 +1,29 @@
+const userSchema = require('../model/user.model')
+const jwt = require('jsonwebtoken')
+
+const veridyAccess = (req, res, next) => {
+  try{
+    let token = req.headers.authorization
+    let decode = jwt.decode(token, process.env.SECRET)
+    const id = req.params.userId
+
+    userSchema.findOne({email: decode.data.email}, (err, user) => {
+      try{
+        if(user.role === 'admin') {
+          console.log('eres admin crack');
+          next()
+        }if(user._id == id){
+          next()
+        }else{
+          res.status(403).send({msg:'You can´t edit another account if it´s not yours'})
+        }
+      }catch(error){
+        res.status(401).send({error: error.message})
+      }
+    })
+  }catch(error){
+    res.status(401).send({ error: error.message })
+  }
+}
+
+module.exports = veridyAccess
