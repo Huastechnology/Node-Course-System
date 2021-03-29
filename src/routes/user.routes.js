ca9afc79036a9aca606167ef3c08cfb3c2593d79
SariMelina <mmkings_beba@hotmail.com>
2021-03-29 12:16:45 -0600

    Fixes to have a good practice

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require('express')
 const api = Router()
 const userController = require('../controller/user.controller')
-const middlewares = require('../middlewares/index')
+const middlewares = require('../middlewares')
 
 api.post('/user',middlewares.verifyRole, middlewares.verifyEmptyFields, middlewares.verifyDuplicatesEmails, userController.saveUser)
 api.post('/logIn', userController.logIn)
