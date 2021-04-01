50a360d43e875223a700e9a74b63e5d0f8fa1b75
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-04-01 13:28:26 -0600

    Student route was created

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 const bp = require ('body-parser')
+
 const route = require('./routes/user.routes')
+const student_route = require('./routes/student.routes')
 
 app.use(bp.urlencoded({extended:false}))
 app.use(bp.json())
 
 app.use('/v2', route)
+app.use('/v2', student_route)
 
 module.exports = app
