7480f0cfeda5539d06b3c6a9232c4059aad1bddd
vicenteyah <vicente.yah.dzul@icloud.com>
2021-04-06 16:18:00 -0500

    the middleware import index was optimized

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,15 +1,8 @@
-const verifyEmptyFields = require('../middlewares/verifyEmptyFields')
-const verifyDuplicatesEmails = require('../middlewares/verifyDuplicatesEmails')
-const verifyRole = require('../middlewares/verifyRole')
-const verifyId = require('../middlewares/verifyId')
-const verifyAuth = require('../middlewares/verifyAuth')
-const verifyUserAccess = require('../middlewares/verifyUserAccess')
-
 module.exports = {
-  verifyEmptyFields,
-  verifyDuplicatesEmails,
-  verifyRole,
-  verifyId,
-  verifyAuth,
-  verifyUserAccess
+  verifyEmptyFields: require('./verifyEmptyFields'),
+  verifyDuplicatesEmails: require('./verifyDuplicatesEmails'),
+  verifyRole: require('./verifyRole') ,
+  verifyId: require('./verifyId'),
+  verifyAuth: require('./verifyAuth'),
+  verifyUserAccess: require('./verifyUserAccess')
 }
