18ca1b8b24237cd1aa3e0851d71e9b2e2a877f28
vicenteyah <vicente.yah.dzul@icloud.com>
2021-04-06 16:22:46 -0500

    using a single instance of the api's routes

--- src/app.js
+++ src/app.js
@@ -2,13 +2,11 @@ const express = require('express')
 const app = express()
 const bp = require ('body-parser')
 
-const route = require('./routes/user.routes')
-const student_route = require('./routes/student.routes')
+const apiSources= require('./routes')
 
 app.use(bp.urlencoded({extended:false}))
 app.use(bp.json())
 
-app.use('/v2', route)
-app.use('/v2', student_route)
+app.use('/v2', apiSources)
 
 module.exports = app
