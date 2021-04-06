d3ea6f978c09e2de8a9e17821fc086901e8f574c
vicenteyah <vicente.yah.dzul@icloud.com>
2021-04-06 16:37:38 -0500

    cors middleware was installed and configured in our app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const bp = require ('body-parser')
-
+const cors = require('cors')
 const apiSources= require('./routes')
 
 app.use(bp.urlencoded({extended:false}))
 app.use(bp.json())
+app.use(cors())
 
 app.use('/v2', apiSources)
 
